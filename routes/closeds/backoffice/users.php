ad9cd8cf8b9043c1bc49cccc0f3357ca5ca78f92
Nicholas Freitas De Souza <nicholaspanonko2@gmail.com>
2025-11-25 08:09:15 -0300

    User crud and user service implemented

--- routes/closeds/backoffice/users.php
+++ routes/closeds/backoffice/users.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\Closeds\Backoffice\UserController;
+use App\Models\User;
 
-Route::get('/users', function () {
-    return "Closeds Backoffice users";
+
+
+Route::controller(UserController::class)->group(function(){
+    Route::prefix("user")->group(function(){
+        Route::get("/","index");
+        Route::get("/{id}","show");
+        Route::post("/","store");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","delete");
+    });
+
+
+
 });
